5f53ae2629d0373b141a604f282e9d001a758fef
Krolicki <jakub.krolicki.k@gmail.com>
2022-12-06 19:14:04 +0100

    Move new stepper motor

--- blinds.py
+++ blinds.py
@@ -1,37 +1,72 @@
 import RPi.GPIO as GPIO
 import time
-
-GPIO.setwarnings(False)
-GPIO.setmode(GPIO.BCM)
-
-PWMA = 17
-AIN2 = 27
-AIN1 = 22
-STBY = 4
-
-# set up GPIO pins
-GPIO.setup(PWMA, GPIO.OUT) 
-GPIO.setup(AIN2, GPIO.OUT) 
-GPIO.setup(AIN1, GPIO.OUT) 
-GPIO.setup(STBY, GPIO.OUT)
-
-
-#start motor
-GPIO.output(AIN1, True)
-GPIO.output(AIN2, False) 
-
-# Set the motor speed
-GPIO.output(PWMA, True) 
-
-# Disable STBY (standby)
-GPIO.output(STBY, True)
-
-print("Start")
-time.sleep(5)
-print("Stop")
-
-# Reset all the GPIO pins by setting them to LOW
-GPIO.output(AIN1, False) 
-GPIO.output(AIN2, False) 
-GPIO.output(PWMA, False) 
-GPIO.output(STBY, False) 
+ 
+in1 = 17
+in2 = 18
+in3 = 27
+in4 = 22
+ 
+# careful lowering this, at some point you run into the mechanical limitation of how quick your motor can move
+step_sleep = 0.002
+ 
+step_count = 4096  #5.625*(1/64) per step, 4096 steps is 360°
+ 
+direction = False # True for clockwise, False for counter-clockwise
+ 
+# defining stepper motor sequence (found in documentation http://www.4tronix.co.uk/arduino/Stepper-Motors.php)
+step_sequence = [[1,0,0,1],
+                 [1,0,0,0],
+                 [1,1,0,0],
+                 [0,1,0,0],
+                 [0,1,1,0],
+                 [0,0,1,0],
+                 [0,0,1,1],
+                 [0,0,0,1]]
+ 
+# setting up
+GPIO.setmode( GPIO.BCM )
+GPIO.setup( in1, GPIO.OUT )
+GPIO.setup( in2, GPIO.OUT )
+GPIO.setup( in3, GPIO.OUT )
+GPIO.setup( in4, GPIO.OUT )
+ 
+# initializing
+GPIO.output( in1, GPIO.LOW )
+GPIO.output( in2, GPIO.LOW )
+GPIO.output( in3, GPIO.LOW )
+GPIO.output( in4, GPIO.LOW )
+ 
+ 
+motor_pins = [in1,in2,in3,in4]
+motor_step_counter = 0 ;
+ 
+ 
+def cleanup():
+    GPIO.output( in1, GPIO.LOW )
+    GPIO.output( in2, GPIO.LOW )
+    GPIO.output( in3, GPIO.LOW )
+    GPIO.output( in4, GPIO.LOW )
+    GPIO.cleanup()
+ 
+ 
+try:
+    i = 0
+    for i in range(step_count):
+        for pin in range(0, len(motor_pins)):
+            GPIO.output( motor_pins[pin], step_sequence[motor_step_counter][pin] )
+        if direction==True:
+            motor_step_counter = (motor_step_counter - 1) % 8
+        elif direction==False:
+            motor_step_counter = (motor_step_counter + 1) % 8
+        else:
+            print( "direction should *always* be either True or False" )
+            cleanup()
+            exit( 1 )
+        time.sleep( step_sleep )
+ 
+except KeyboardInterrupt:
+    cleanup()
+    exit( 1 )
+ 
+cleanup()
+exit( 0 )
