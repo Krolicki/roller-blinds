b516e0709a25d190a98f7bfed70265e0e45898b5
Krolicki <jakub.krolicki.k@gmail.com>
2022-12-20 22:16:12 +0100

    Pokazywanie stopnia i procesu otwarcia w czasie rzeczywistym

--- blinds.py
+++ blinds.py
@@ -1,7 +1,7 @@
 import RPi.GPIO as GPIO
 import time
 import threading
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
  
 in1 = 17
 in2 = 18
@@ -10,12 +10,12 @@ in4 = 22
  
 # careful lowering this, at some point you run into the mechanical limitation of how quick your motor can move
 #step_sleep = 0.002
-step_sleep = 0.2
+step_sleep = 0.02
  
 full_step = 4096  #5.625*(1/64) per step, 4096 steps is 360°, 1 step = +-5 cm
 
-max_step = 151552 #4096 * 37
- 
+#max_step = 151552 #4096 * 37
+max_step = 1000
 # defining stepper motor sequence (found in documentation http://www.4tronix.co.uk/arduino/Stepper-Motors.php)
 step_sequence = [[1,0,0,1],
                  [1,0,0,0],
@@ -54,8 +54,19 @@ def cleanup():
     GPIO.output( in3, GPIO.LOW )
     GPIO.output( in4, GPIO.LOW )
     #GPIO.cleanup()
- 
- 
+        
+def read_steps():
+    global steps_count
+    f = open("/home/pi/scripts/roller-blinds/steps_count.txt","r")
+    steps_count = int(f.readline())
+    f.close()
+        
+def save_steps():
+    global steps_count
+    f = open("/home/pi/scripts/roller-blinds/steps_count.txt","w")#write mode
+    f.write(str(steps_count))
+    f.close()
+
 def roll(steps, direction):
     global steps_count
     global move
@@ -90,23 +101,11 @@ def roll(steps, direction):
             cleanup()
             save_steps()
             break
-        
-def read_steps():
-    global steps_count
-    f = open("/home/pi/scripts/roller-blinds/steps_count.txt","r")
-    steps_count = int(f.readline())
-    f.close()
-        
-def save_steps():
-    global steps_count
-    f = open("/home/pi/scripts/roller-blinds/steps_count.txt","w")#write mode
-    f.write(str(steps_count))
-    f.close()
 
 @app.route('/')
 def index():
     global steps_count
-    return render_template('index.html', steps = steps_count)
+    return render_template('index.html', steps = steps_count, maxStep = max_step, sleep = step_sleep)
 
 @app.route("/roll-up", methods=["POST"])
 def roll_up():
@@ -126,7 +125,7 @@ def roll_down():
     if(move == True):
         move = False
         time.sleep( step_sleep )
-    x = threading.Thread(target=roll, args=(10000-steps_count,'down'))#max_step-steps_count
+    x = threading.Thread(target=roll, args=(max_step-steps_count,'down'))
     x.start()
     return str(steps_count)
 
@@ -138,6 +137,13 @@ def abort():
         cleanup()
         save_steps()
     return str(steps_count)
+
+@app.route('/steps', methods=['GET'])
+def steps():
+    global steps_count
+    global move
+    #return str(steps_count)
+    return jsonify({'step': steps_count,'move': move})
 
 try:
      print("start")
--- static/script.js
+++ static/script.js
@@ -1,4 +1,9 @@
-console.log("start")
+const rollUpButton = document.getElementById('roll-up')
+const rollDownButton = document.getElementById('roll-down')
+const abortButton = document.getElementById('abort')
+const stepsField = document.getElementById('steps')
+
+let rollInterval = null
 
 const makeRequest = (action) => {
 	console.log(action)
@@ -7,12 +12,60 @@ const makeRequest = (action) => {
 				request.onload = () => {
 						const response = request
 						console.log(response)
-						document.getElementById('steps').innerHTML = response.responseText
 				}; 
 				request.send()
 }
 
+const getSteps = async () => {
+		let currentStep = await fetch('/steps')
+		.then(response => {
+				if (response.ok)
+						return response.json()
+				throw response
+		})
+		.catch((err)=>{
+			console.log(err)
+		})
+		console.log(currentStep)
+		return currentStep
+}
 
-document.querySelectorAll('button').forEach(button => {
-		button.addEventListener("click", () => makeRequest(button.id))
+const getRollProgress = async () =>{
+		let currentStep = await getSteps()
+				console.log(currentStep)
+		if(currentStep.move == true){
+			let progress = ((currentStep.step / maxStep) * 100).toFixed(0)
+			stepsField.innerHTML = `Stopień otwarcia: ${progress}%`
+			setTimeout(getRollProgress, 2000);
+		}
+		else{
+			if(currentStep.step == 0)
+				stepsField.innerHTML = 'Status: Otwarte'
+			else if(currentStep.step == maxStep)
+				stepsField.innerHTML = 'Status: Zamknięte'
+			else{
+				let progress = ((currentStep.step / maxStep) * 100).toFixed(0)
+				stepsField.innerHTML = `Stopień otwarcia: ${progress}%`
+			}
+		}
+}
+
+rollUpButton.addEventListener("click", () => {
+			makeRequest('roll-up')
+			getRollProgress()
 })
+rollDownButton.addEventListener("click", () => {
+			makeRequest('roll-down')
+			getRollProgress()
+})
+abortButton.addEventListener("click", () => {
+			makeRequest('abort')
+			getRollProgress()
+})
+//document.querySelector('.buttons-wraper').querySelectorAll('button')forEach(button => {
+		//button.addEventListener("click", () => {
+			//makeRequest(button.id)
+			//getRollProgress()
+		//})
+//})
+getRollProgress()
